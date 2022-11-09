a0e0f2cf3a816e1a7fd7e26859ebbfa7b74ac774
panvova <panvovandrik@gmail.com>
2022-11-09 12:35:48 -0800

    task4

---
+++ src/main/kotlin/task4.py
@@ -0,0 +1,45 @@
+import math
+import time
+from mpi4py import MPI
+import numpy as np
+from numpy import ndarray
+
+def monteCarloMethod(array):
+    # transpose method for array in numpy library
+    # then we calculate our part of array squared random x,y and taking root
+    # of it and check when the value will be lower or equal to 1
+    circlePoints = np.count_nonzero((pow(array.T[0], 2) + pow(array.T[1], 2)) ** 0.5 <= 1)
+    squarePoints = len(array)
+    pi = (circlePoints / squarePoints) * 4
+    return pi
+
+
+if __name__ == "__main__":
+    mpiCommWorld = MPI.COMM_WORLD
+    mpiSize, mpiRank = (mpiCommWorld.Get_size(), mpiCommWorld.Get_rank())
+    n = 100000
+
+    print("MPI SIZE: " + str(mpiSize))
+    print("MPI RANK: " + str(mpiRank) + "\n")
+
+    parts = math.ceil(n / mpiSize)  # divide by sizes
+    arr: ndarray = np.random.rand(n, 2)  # generate numbers
+
+    data = []
+    for i in range(0, mpiSize):
+        data.append(arr[0: (i + 1) * parts])
+
+    # will scatter the data to specific channels by parts
+    dots = mpiCommWorld.scatter(data, root=0)
+
+    # calculate the time
+    startTime = time.time()
+
+    piNumber = monteCarloMethod(dots)
+
+    endTime = time.time()
+
+    results = mpiCommWorld.gather(piNumber, root=0)
+
+    for result in results:
+        print(result)
